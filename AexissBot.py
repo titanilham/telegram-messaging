518f4a2f08bb17a32e59d1b743b750bc6efaba92
Ilham <86422270+titanilham@users.noreply.github.com>
2025-09-20 11:28:41 +0300

    Update AexissBot.py

--- AexissBot.py
+++ AexissBot.py
@@ -1,12 +1,13 @@
 import requests
 
+TOKEN = "********************************"
+CHAT_ID = "*************"
 
-def send_msg(text):
-    token = "**************************"
-    chat_id = "**********"
+def send_msg(text: str, token: str, chat_id: str ) -> None:
+   
     url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text
     results = requests.get(url_req)
     print(results.json())
 
-
-send_msg("HI")
+if __name__ == "__main__":
+    send_msg("HI", TOKEN, CHAT_ID)
